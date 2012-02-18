0ed1864109ac97c66f5d18e567cc9b96038da947
Farkas Ferencz <fferencz@gmail.com>
2012-02-18 18:09:40 +0200

    fixing spacing

--- OFC/Elements/OFCArrow.php
+++ OFC/Elements/OFCArrow.php
@@ -10,40 +10,40 @@ namespace WW\OFCBundle\OFC\Elements;
 
 class OFCArrow
 {
-  
+
   private $type;
   private $start;
   private $end;
   private $colour;
   private $barb_length;
-  
-	/**
-	 * @param int $x Start x position
-	 * @param int $y Start y position
-	 * @param int $a End x position
-	 * @param int $b End y position
-	 * @param string $color HTML HEX color format
-	 * @param int $barb_length Length of the barbs in pixels.
-	 */
-	function __construct($x, $y, $a, $b, $color, $barb_length=10)
-	{
-		$this->type   = "arrow";
-		$this->start	= array("x" => $x, "y"=> $y);
-		$this->end		= array("x "=> $a, "y"=> $b);
-		$this->setColor($color);
-		$this->barb_length = $barb_length;
-	}
-	
+
+  /**
+   * @param int $x Start x position
+   * @param int $y Start y position
+   * @param int $a End x position
+   * @param int $b End y position
+   * @param string $color HTML HEX color format
+   * @param int $barb_length Length of the barbs in pixels.
+   */
+  function __construct($x, $y, $a, $b, $color, $barb_length=10)
+  {
+    $this->type   = "arrow";
+    $this->start	= array("x" => $x, "y"=> $y);
+    $this->end		= array("x "=> $a, "y"=> $b);
+    $this->setColor($color);
+    $this->barb_length = $barb_length;
+  }
+
   /**
    * Set the color
    * 
    * @param type $color HTML HEX color format   * 
    * @return OFCArrow 
    */
-	public function setColor($color)
-	{
-		$this->colour = $color;
-    
-		return $this;
-	}
+  public function setColor($color)
+  {
+    $this->colour = $color;
+
+    return $this;
+  }
 }
