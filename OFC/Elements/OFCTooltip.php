87dbdcf6dd1c15ee51aeab940b49499d28343dc3
Farkas Ferencz <fferencz@gmail.com>
2012-02-18 18:11:01 +0200

    fixing spacing

--- OFC/Elements/OFCTooltip.php
+++ OFC/Elements/OFCTooltip.php
@@ -16,94 +16,94 @@ class OFCTooltip
   private $title;
   private $body;
   private $mouse;
-  
-	/**
-	 * @param $shadow as boolean. Enable drop shadow.
+
+  /**
+   * @param $shadow as boolean. Enable drop shadow.
    * @return OFCTooltip
-	 */
-	public function setShadow($shadow)
-	{
-		$this->shadow = $shadow;
-    
+   */
+  public function setShadow($shadow)
+  {
+    $this->shadow = $shadow;
+
     return $this;
-	}
-	
-	/**
-	 * @param $stroke as integer, border width in pixels (e.g. 5)
+  }
+
+  /**
+   * @param $stroke as integer, border width in pixels (e.g. 5)
    * @return OFCTooltip
-	 */
-	public function setStroke($stroke)
-	{
-		$this->stroke = $stroke;
-    
+   */
+  public function setStroke($stroke)
+  {
+    $this->stroke = $stroke;
+
     return $this;
-	}
-	
-	/**
-	 * @param $colour as string, HEX colour e.g. '#0000ff'
+  }
+
+  /**
+   * @param $colour as string, HEX colour e.g. '#0000ff'
    * @return OFCTooltip
-	 */
-	public function setColor($color)
-	{
-		$this->color = $color;
-    
+   */
+  public function setColor($color)
+  {
+    $this->color = $color;
+
     return $this;
-	}
-	
-	/**
-	 * @param $bg as string, HEX colour e.g. '#0000ff'
+  }
+
+  /**
+   * @param $bg as string, HEX colour e.g. '#0000ff'
    * @return OFCTooltip
-	 */
-	public function setBackgroundColor($bg)
-	{
-		$this->background = $bg;
-    
+   */
+  public function setBackgroundColor($bg)
+  {
+    $this->background = $bg;
+
     return $this;
-	}
-	
-	/**
-	 * @param $style as string. A css style.
+  }
+
+  /**
+   * @param $style as string. A css style.
    * @return OFCTooltip
-	 */
-	public function setTitleStyle($style)
-	{
-		$this->title = $style;
-    
+   */
+  public function setTitleStyle($style)
+  {
+    $this->title = $style;
+
     return $this;
-	}
-	
-	/**
-	 * @param $style as string. A css style.
+  }
+
+  /**
+   * @param $style as string. A css style.
    * @return OFCTooltip
-	 */
+   */
   public function setBodyStyle($style)
-	{
-		$this->body = $style;
-    
+  {
+    $this->body = $style;
+
     return $this;
-	}
-	
+  }
+
   /**
    * Set the mouse proximity
    * 
    * @return OFCTooltip 
    */
-	public function setProximity()
-	{
-		$this->mouse = 1;
-    
+  public function setProximity()
+  {
+    $this->mouse = 1;
+
     return $this;
-	}
-	
+  }
+
   /**
    * Set hover
    * 
    * @return OFCTooltip 
    */
-	public function setHover()
-	{
-		$this->mouse = 2;
-    
+  public function setHover()
+  {
+    $this->mouse = 2;
+
     return $this;
-	}
+  }
 }
